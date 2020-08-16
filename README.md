66498237f7acdbecc893f36ca3ab4f4801cbdb3e
刘凯 <354378485@qq.com>
2020-08-16 22:42:22 +0800

    登录 - 表单验证

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-> 哔哩哔哩视频看到https://www.bilibili.com/video/BV1Uz411i7y7?p=28
+> 哔哩哔哩视频看到https://www.bilibili.com/video/BV1Uz411i7y7?p=36
---
+++ src/components/Star/Star.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="star" :class="'star-'+size" >
+    <span class="star-item" :class="item" v-for="(item,index) in starClasses" :key="index"></span>
+  </div>
+</template>
+
+<script>
+const CLASS_ON = 'on'
+const CLASS_OFF = 'off'
+const CLASS_HALF = 'half'
+export default {
+    props:{
+        score:Number,
+        size:Number,        
+    },
+    computed:{
+        starClasses(){
+            const {score} = this;
+            const scs = [];
+            const scoreInteger = Math.floor(score);
+            for(let i = 0;i < scoreInteger; i++){
+                scs.push(CLASS_ON);
+            }
+            if(score*10 - scoreInteger*10 >= 5){
+                scs.push(CLASS_HALF);
+            }
+            while (scs.length < 5) {
+                scs.push(CLASS_OFF);
+            }
+
+            return scs;
+        }
+    }
+};
+</script>
+
+<style lang="scss">
+@import "../../assets/mixins.scss";
+.star {
+  //2x图 3x图
+  float: left;
+  font-size: 0;
+  .star-item {
+    display: inline-block;
+    background-repeat: no-repeat;
+  }
+  &.star-48 {
+    .star-item {
+      width: 20px;
+      height: 20px;
+      margin-right: 22px;
+      background-size: 20px 20px;
+      &:last-child {
+        margin-right: 0;
+      }
+      &.on {
+        @include bg-image("./images/star48_on");
+      }
+      &.half {
+        @include bg-image("./images/star48_half");
+      }
+      &.off {
+        @include bg-image("./images/star48_off");
+      }
+    }
+  }
+  &.star-36 {
+    .star-item {
+      width: 15px;
+      height: 15px;
+      margin-right: 6px;
+      background-size: 15px 15px;
+      &:last-child {
+        margin-right: 0;
+      }
+      &.on {
+        @include bg-image("./images/star36_on");
+      }
+      &.half {
+        @include bg-image("./images/star36_half");
+      }
+      &.off {
+        @include bg-image("./images/star36_off");
+      }
+    }
+  }
+  &.star-24 {
+    .star-item {
+      width: 10px;
+      height: 10px;
+      margin-right: 3px;
+      background-size: 10px 10px;
+      &:last-child {
+        margin-right: 0;
+      }
+      &.on {
+        @include bg-image("./images/star24_on");
+      }
+      &.half {
+        @include bg-image("./images/star24_half");
+      }
+      &.off {
+        @include bg-image("./images/star24_off");
+      }
+    }
+  }
+}
+</style>

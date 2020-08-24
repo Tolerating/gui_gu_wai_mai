27bd547c60dfd8b06b94912035d112447d27515f
刘凯 <354378485@qq.com>
2020-08-25 00:11:12 +0800

    商家信息

--- src/pages/Search/Search.vue
+++ src/pages/Search/Search.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="search">
-    <HeaderTop title="搜索"/>
+    <HeaderTop title="搜索" />
     <form class="search_form" action="#">
       <input
         type="search"
@@ -10,23 +10,56 @@
       />
       <input type="submit" name="submit" value="提交" class="search_submit" />
     </form>
+    <section class="list">
+      <ul class="list_container">
+        <li class="list_li">
+          <section class="item_left">
+            <img
+              src="http://cangdu.org:8001/img/16265a70fe27854.jpg"
+              class="restaurant_img"
+            />
+          </section>
+          <section class="item_right">
+            <div class="item_right_text">
+              <p><span>aaa</span></p>
+              <p>月售 671 单</p>
+              <p>20 元起送 / 距离 1058.2 公里</p>
+            </div>
+          </section>
+        </li>
+        <li class="list_li">
+          <section class="item_left">
+            <img
+              src="http://cangdu.org:8001/img/16265a70fe27854.jpg"
+              class="restaurant_img"
+            />
+          </section>
+          <section class="item_right">
+            <div class="item_right_text">
+              <p><span>aaa</span></p>
+              <p>月售 671 单</p>
+              <p>20 元起送 / 距离 1058.2 公里</p>
+            </div>
+          </section>
+        </li>
+      </ul>
+    </section>
   </div>
 </template>
 
 <script>
-import HeaderTop from '../../components/HeaderTop/HeaderTop'
+import HeaderTop from "../../components/HeaderTop/HeaderTop";
 export default {
-    components:{
-        HeaderTop
-    }
+  components: {
+    HeaderTop,
+  },
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 @import "../../assets/mixins.scss";
 .search {
   width: 100%;
-  
   .search_form {
     @include clearFix();
     margin-top: 45px;
@@ -55,6 +88,43 @@ export default {
         background-color: #02a774;
       }
     }
+  }
+  .list {
+    .list_container {
+      background-color: #fff;
+      .list_li {
+        display: flex;
+        justify-content: center;
+        padding: 10px;
+        border-bottom: 1px solid $bc;
+        .item_left {
+          margin-right: 10px;
+          .restaurant_img {
+            width: 50px;
+            height: 50px;
+            display: block;
+          }
+        }
+        .item_right {
+          font-size: 12px;
+          flex: 1;
+          .item_right_text p {
+            line-height: 12px;
+            margin-bottom: 6px;
+            &:last-child {
+              margin-bottom: 0;
+            }
+          }
+        }
+      }
+    }
+  }
+  .search_none {
+    margin: 0 auto;
+    color: #333;
+    background-color: #fff;
+    text-align: center;
+    margin-top: 0.125rem;
   }
 }
 </style>
